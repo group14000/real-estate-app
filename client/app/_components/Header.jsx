feac22ca7d76a858fb8de0c35cd8709310eea588
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-04-10 02:19:30 +0530

    commit: authentication

--- client/app/_components/Header.jsx
+++ client/app/_components/Header.jsx
@@ -1,16 +1,30 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const Header = () => {
+  const path = usePathname();
+  const { user, isSignedIn } = useUser();
+
+  useEffect(() => {
+    console.log(path);
+  }, []);
   return (
     <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
       <div className="flex gap-12 items-center">
         <Image src={"/logo.svg"} width={150} height={150} alt="logo" />
         <ul className="hidden md:flex gap-10">
-          <li className="hover:text-primary font-medium text-sm cursor-pointer">
-            For Sale
-          </li>
+          <Link href={"/"}>
+            <li className="hover:text-primary font-medium text-sm cursor-pointer">
+              For Sale
+            </li>
+          </Link>
+
           <li className="hover:text-primary font-medium text-sm cursor-pointer">
             For Rent
           </li>
@@ -19,12 +33,18 @@ const Header = () => {
           </li>
         </ul>
       </div>
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <Button className="flex gap-2">
           <Plus className="h-5 w-5" />
           Post Your Ad
         </Button>
-        <Button variant="outline">Login</Button>
+        {isSignedIn ? (
+          <UserButton />
+        ) : (
+          <Link href={"/sign-in"}>
+            <Button variant="outline">Login</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
