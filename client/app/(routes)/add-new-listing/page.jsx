a7a889f473ebe56eb980ab64a4a5e38ea571705c
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-04-12 00:44:10 +0530

    commit: mapbox-2

--- client/app/(routes)/add-new-listing/page.jsx
+++ client/app/(routes)/add-new-listing/page.jsx
@@ -3,11 +3,15 @@ import React from "react";
 
 const AddNewListing = () => {
   return (
-    <div className="p-10 flex flex-col gap-5 items-center justify-center">
-      <h2 className="font-bold text-2xl">Add New Listing</h2>
-      <div>
-        <h2 className="text-gray-500">Enter Address which you want to list</h2>
-        <MapboxAddressSearch />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-10 flex flex-col gap-5 items-center justify-center">
+        <h2 className="font-bold text-2xl">Add New Listing</h2>
+        <div className="text-center">
+          <h2 className="text-gray-500">
+            Enter Address which you want to list
+          </h2>
+          <MapboxAddressSearch />
+        </div>
       </div>
     </div>
   );
--- client/app/_components/MapboxGeocoder.jsx
+++ client/app/_components/MapboxGeocoder.jsx
@@ -1,14 +1,15 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 const MapboxAddressSearch = () => {
   const mapContainer = useRef(null);
 
   useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
+
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
@@ -19,27 +20,22 @@ const MapboxAddressSearch = () => {
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl,
-      countries: "US",
-      placeholder: "Search property address",
-      onChange:(place)=>{
-        console.log(place);
-      }
+      placeholder: "Search for a place",
+      marker: false,
     });
 
     map.addControl(geocoder);
 
-    geocoder.on("result", (result) => {
-      console.log(result);
-    });
-
-    geocoder.on("error", (error) => {
-      console.error(error);
-    });
-
     return () => map.remove();
   }, []);
 
-  return <div ref={mapContainer} className="w-full h-96 md:h-72 lg:h-96" />;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full h-full p-8">
+        <div ref={mapContainer} className="w-full h-full" />
+      </div>
+    </div>
+  );
 };
 
 export default MapboxAddressSearch;
